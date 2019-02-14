c8fc03efc03880207f93a8faa818b7686d107e3b
EKN <EKN@delegate.dk>
2019-02-14 08:52:39 +0100

    #29 - Cleanup unused files and packages

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { RouteComponentProps } from 'react-router';
 import icon from "../assets/logo.svg";
-import fs from "fs-extra";
-export interface IHomeProps {
 
-}
-
-export interface IHomeState {
-
-}
-
-export default class Home extends React.Component<IHomeProps, IHomeState>{
+export default class Home extends React.Component{
     render() {
         return (
             <div>
