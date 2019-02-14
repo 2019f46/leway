2699144b28543754eddbe5c650f1d1f661bed681
Junge <201305945@post.au.dk>
2019-02-14 10:05:46 +0100

    #30 - Adding blank settings component

--- src/routing/ProjectRouter.tsx
+++ src/routing/ProjectRouter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch, Redirect, BrowserRouter, MemoryRouter } from 'react-router-dom';
 import Home from "../components/Home";
 import PageNotFound from "../components/pagenotfound/PageNotFound";
+import SettingsPage from "../components/settingspage/SettingsPage";
 import createBrowserHistory from 'history/createBrowserHistory'
 
 export class ProjectRouter extends React.Component{
@@ -13,6 +14,7 @@ export class ProjectRouter extends React.Component{
                     <Switch>
                         <Route exact path='/home' component={Home} />
                         <Redirect exact from="/" to="/home" />
+                        <Route exact path='/settings' component={SettingsPage}/>
                         <Route component={PageNotFound} />
                     </Switch>
                 </div>
