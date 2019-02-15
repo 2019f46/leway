03c097f79f21ab11904c852e43ada4abcc1019e7
EKN <EKN@delegate.dk>
2019-02-15 08:56:53 +0100

    #31 map service test file

---
+++ tests/MapService.unit.test.ts
@@ -0,0 +1,6 @@
+import MapService, { IMapService } from "../src/services/MapService";
+import axios from 'axios';
+
+it('MapService gets data', async () => {
+    expect("result").toEqual(true);
+});
