476d9c46fe22848dd0aa946cab5db4b7d831a927
EKN <EKN@delegate.dk>
2019-02-23 16:26:27 +0100

    inprogress 404

--- src/components/pagenotfound/PageNotFound.tsx
+++ src/components/pagenotfound/PageNotFound.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Image } from "office-ui-fabric-react/lib/Image";
+import styles from "./PageNotFound.module.scss";
+import { Link } from "office-ui-fabric-react/lib/Link";
+
 
 export default class PageNotFound extends React.Component {
+    private image = require("../../assets/404.png");
     public render() {
         return (
-            <h1>
-                Page Not Found
-            </h1>
+            <div className={styles.notFoundContainer}>
+                <Image src={this.image} />
+                <Link href={window.location.origin} />
+            </div>
         );
     }
 }
