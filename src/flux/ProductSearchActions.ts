db0612697af2bf264e7818d6f09fbe9215d23f4f
Eduardo Knudsen <eduardoknudsen@gmail.com>
2019-03-10 12:26:09 +0100

    #44 - documentation

--- src/flux/ProductSearchActions.ts
+++ src/flux/ProductSearchActions.ts
@@ -1,7 +1,15 @@
 import dispatcher from "./Dispatchers";
 import { IProduct } from "../models/ProductModel";
 
+/**
+ * This are the actions which components who can alter the ProductSearchStore can use
+ */
 class ProductSearchActions {
+
+    /**
+     * This method crates a action object which is sent to the store to handle
+     * @param products Products to set
+     */
     public setProducts(products: IProduct[]): void {
         dispatcher.dispatch({
             type: "SET_PRODUCTS",
@@ -9,6 +17,10 @@ class ProductSearchActions {
         });
     }
 
+    /**
+     * This method crates a action object which is sent to the store to handle
+     * @param products Product to set
+     */
     public setSelectedProduct(product: IProduct): void {
         dispatcher.dispatch({
             type: "SET_SELECTED",
