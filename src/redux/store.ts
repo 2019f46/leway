5d0d9313c9b0ed42f22877ba3874bc96dc708771
Eduardo Knudsen <eduardoknudsen@gmail.com>
2019-04-19 13:13:15 +0200

    This should fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
-
-const isLocalhost = Boolean(
-    window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
-let data;
-if (isLocalhost) {
-    data = require('redux-devtools-extension');
-}
 
 const initialState = {}
 
@@ -22,7 +9,7 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer,
     initialState,
-    !isLocalhost ? applyMiddleware(...middleware) : data.composeWithDevTools(
+    composeWithDevTools(
         applyMiddleware(...middleware)
     )
 
