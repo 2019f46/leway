59b2c36db3b9ceef7eb0c16ea8c3b49d5eda163b
Eduardo Knudsen <eduardoknudsen@gmail.com>
2019-04-19 10:37:24 +0200

    redux fix

--- src/redux/productsearch/ProductSearchActions.ts
+++ src/redux/productsearch/ProductSearchActions.ts
@@ -1,5 +1,7 @@
 import { IProduct } from "../../models/ProductModel";
-import { SET_SELECTED_PRODUCT, SET_PRODUCT_LIST, SET_SEARCH_VALUE } from "../types";
+import FakeSearchService from "../../services/fakes/FakeSearchService";
+import SearchService, { ISearchService } from "../../services/SearchService";
+import { SET_PRODUCT_LIST, SET_SEARCH_VALUE, SET_SELECTED_PRODUCT } from "../types";
 
 export function setSelectedProduct(selectedProduct: IProduct) {
     return function (dispatch: any) {
@@ -10,17 +12,29 @@ export function setSelectedProduct(selectedProduct: IProduct) {
     }
 }
 
-export function setProductList(list: IProduct[]) {
-    return function (dispatch: any) {
-        dispatch({
-            type: SET_PRODUCT_LIST,
-            data: list
-        });
+export function setProductList(query?: string, fake?: boolean) {
+    if (query) {
+        let searchService: ISearchService = fake ? new FakeSearchService() : new SearchService();
+        return function (dispatch: any) {
+            searchService.getProduct(query).then(products => {
+                dispatch({
+                    type: SET_PRODUCT_LIST,
+                    data: products
+                });
+            });
+        }
+    } else {
+        return function (dispatch: any) {
+            dispatch({
+                type: SET_PRODUCT_LIST,
+                data: []
+            });
+        }
     }
 }
 
-export function setSearchValue(value: string){
-    return function(dispatch:any){
+export function setSearchValue(value: string) {
+    return function (dispatch: any) {
         dispatch({
             type: SET_SEARCH_VALUE,
             data: value
