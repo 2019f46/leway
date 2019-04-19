d265ed2231c87c0ccef88c404467812e0b7b25a4
Eduardo Knudsen <eduardoknudsen@gmail.com>
2019-04-19 13:03:14 +0200

    placeholder test

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
-// import { composeWithDevTools } from 'redux-devtools-extension';
+
+const isLocalhost = Boolean(
+    window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+let data;
+if (isLocalhost) {
+    data = require('redux-devtools-extension');
+}
 
 const initialState = {}
 
@@ -9,9 +22,10 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer,
     initialState,
-    // composeWithDevTools(
-    //     applyMiddleware(...middleware)
-    // )
+    !isLocalhost ? applyMiddleware(...middleware) : data.composeWithDevTools(
+        applyMiddleware(...middleware)
+    )
+
 );
 
 export default store;
